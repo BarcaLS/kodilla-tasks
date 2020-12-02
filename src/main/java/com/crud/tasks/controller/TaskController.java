a531f5772c77f3b0748319b3d725ad9fb42094e6
BarcaLS <marcin@sarna.info>
2020-12-02 16:35:01 +0100

    restriction to controller in Swagger

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -9,7 +9,6 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/v1/task")
