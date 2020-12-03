7785697d481cf11f1d02c51bb1a3c11a3ad845a4
BarcaLS <marcin@sarna.info>
2020-12-03 23:24:05 +0100

    zmiany

--- src/main/java/com/crud/tasks/domain/Mail.java
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -6,9 +6,10 @@ import lombok.Getter;
 
 @Builder
 @Getter
+@AllArgsConstructor
 public class Mail {
     private final String mailTo;
-    private final String toCc;
     private final String subject;
     private final String message;
+    private final String toCc;
 }
